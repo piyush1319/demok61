cf0a5a1e46f3da8b772f78b6e7b0cb497230c0eb
piyush1319 <32597204+piyush1319@users.noreply.github.com>
2023-02-12 19:08:42 +0530

    Update setup_k6.sh

--- setup_k6.sh
+++ setup_k6.sh
@@ -2,8 +2,8 @@
 set -ex
 
 sudo apt-get update
-sudo apt-get install dirmngr --install-recommends
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 379CE192D401AB61
+sudo -S apt-get install dirmngr --install-recommends
+sudo -S apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 379CE192D401AB61
 echo "deb https://dl.bintray.com/loadimpact/deb stable main" | sudo tee -a /etc/apt/sources.list
-sudo apt-get update
-sudo apt-get install k6
+sudo -S apt-get update
+sudo -S apt-get install k6
